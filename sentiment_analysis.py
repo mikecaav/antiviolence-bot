b864fa31d234a651a86bd528715d413b35182858
Miguel Caballero <caballero.mc98@gmail.com>
2021-06-28 22:11:25 -0500

    Refactored sentiment analysis file

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,7 +6,14 @@ import csv
 import urllib.request
 
 
-# Preprocess text (username and link placeholders)
+def get_labels():
+    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/emotion/mapping.txt"
+    with urllib.request.urlopen(mapping_link) as f:
+        html = f.read().decode('utf-8').split("\n")
+        csvreader = csv.reader(html, delimiter='\t')
+    return [row[1] for row in csvreader if len(row) > 1]
+
+
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
@@ -15,40 +22,34 @@ def preprocess(text):
         new_text.append(t)
     return " ".join(new_text)
 
-# Tasks:
-# emoji, emotion, hate, irony, offensive, sentiment
-# stance/abortion, stance/atheism, stance/climate, stance/feminist, stance/hillary
 
-task='emotion'
-MODEL = f"twitter-roberta-base-emotion"
+class SentimentAnalysisModel:
+    def __init__(self):
+        self.MODEL = "twitter-roberta-base-emotion"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.MODEL)
+        self.labels = get_labels()
+        self.model = self.init_model()
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    def init_model(self):
+        return AutoModelForSequenceClassification.from_pretrained(self.MODEL)
 
-# download label mapping
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/{task}/mapping.txt"
-with urllib.request.urlopen(mapping_link) as f:
-    html = f.read().decode('utf-8').split("\n")
-    csvreader = csv.reader(html, delimiter='\t')
-labels = [row[1] for row in csvreader if len(row) > 1]
+    def get_scores(self, text):
+        text = preprocess(text)
+        encoded_input = self.tokenizer(text, return_tensors='pt')
+        output = self.model(**encoded_input)
+        scores = output[0][0].detach().numpy()
+        scores = softmax(scores)
+        ranking = np.argsort(scores)
+        ranking = ranking[::-1]
+        for i in range(scores.shape[0]):
+            label = self.labels[ranking[i]]
+            score = scores[ranking[i]]
+            probability = np.round(float(score), 4)
+            print(f"{i + 1}) {label} {probability}")
 
-model = AutoModelForSequenceClassification.from_pretrained("twitter-roberta-base-emotion")
 
-text = "Cannot believe that you're saying that 🙄"
-text = preprocess(text)
-encoded_input = tokenizer(text, return_tensors='pt')
-output = model(**encoded_input)
-scores = output[0][0].detach().numpy()
-scores = softmax(scores)
 
-# text = "Celebrating my promotion 😎"
-# encoded_input = tokenizer(text, return_tensors='tf')
-# output = model(encoded_input)
-# scores = output[0][0].numpy()
-# scores = softmax(scores)
 
-ranking = np.argsort(scores)
-ranking = ranking[::-1]
-for i in range(scores.shape[0]):
-    l = labels[ranking[i]]
-    s = scores[ranking[i]]
-    print(f"{i+1}) {l} {np.round(float(s), 4)}")
+if __name__ == "__main__":
+    model = SentimentAnalysisModel()
+    model.get_scores("Cannot believe that you're saying that 🙄")

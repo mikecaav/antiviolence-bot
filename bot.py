8a91e4835dd5f0ad1a26f53b41ca50e92a10a88e
Miguel Caballero <caballero.mc98@gmail.com>
2021-06-29 21:25:58 -0500

    Created a moderator role and hidden the reported-messages channel for general role

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ client = discord.Client()
 model = EmotionAnalysisModel()
 CHANNEL_CATEGORY = 'anti-violence-bot'
 CHANNEL_NAME = 'reported-messages'
+MODERATOR_ROLE = 'moderator message reviewer'
 
 @client.event
 async def on_ready():
@@ -26,13 +27,36 @@ async def on_message(message):
 async def on_guild_join(guild):
     categories = get_categories(guild)
     channels = get_channels(guild)
-    print("Your bot just landed into a new server! We've created the corresponding channels to make it work :D")
+    roles = get_roles(guild)
+
+    print(f"Your bot just landed into {guild.name}! We've created the corresponding channels, roles and categories to make it work :D")
     if CHANNEL_CATEGORY not in categories:
         category = await guild.create_category(CHANNEL_CATEGORY)
+
+    if MODERATOR_ROLE not in roles:
+        permissions = discord.Permissions(
+            ban_members=True,
+            kick_members=True,
+            deafen_members=True,
+            mute_members=True,
+            read_messages=True
+        )
+        moderator_role = await guild.create_role(
+            name=MODERATOR_ROLE,
+            permissions=permissions,
+            mentionable=True
+        )
+
     if CHANNEL_NAME not in channels:
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            moderator_role: discord.PermissionOverwrite(read_messages=True)
+        }
         await guild.create_text_channel(
             CHANNEL_NAME,
-            category=category
+            category=category,
+            topic="This text channel was created as a special place to review the messages that are detected as hostile to decide what to do with the hostile users",
+            overwrites=overwrites
         )
 
 
@@ -43,6 +67,8 @@ def get_categories(guild):
 def get_channels(guild):
     return get_objects_from_list(guild.channels)
 
+def get_roles(guild):
+    return get_objects_from_list(guild.roles)
 
 def get_objects_from_list(list_of_objects):
     objects = set()

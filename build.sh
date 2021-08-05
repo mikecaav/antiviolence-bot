6b9d9bed03b3d4ca51a909617c69239950bf8d21
Miguel Caballero <caballero.mc98@gmail.com>
2021-08-04 20:32:27 -0500

    added build file

---
+++ build.sh
@@ -0,0 +1,8 @@
+FILE=emotion_analysis.py
+if test -f "$FILE"; then
+  echo "$FILE exists."
+fi
+#apt-get -y install git-lfs
+#git lfs install
+#git clone https://huggingface.co/cardiffnlp/twitter-roberta-base-emotion
+#docker build -t mikex40/antiviolence-bot -f Dockerfile .

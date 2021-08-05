11f9087bd0e14f2335040a96c2e118ab266e4faf
Miguel Caballero <caballero.mc98@gmail.com>
2021-08-04 20:41:27 -0500

    added optional download for emotion analysis model

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
-FILE=emotion_analysis.py
+FILE=twitter-roberta-base-emotion/pytorch_model.bin
+
+apt-get -y install git-lfs
+git lfs install
+
 if test -f "$FILE"; then
   echo "$FILE exists."
+else
+  echo "$FILE doesn't exists, downloading files..."
+  git clone https://huggingface.co/cardiffnlp/twitter-roberta-base-emotion
+  echo "$FILE is now downloaded"
 fi
-#apt-get -y install git-lfs
-#git lfs install
-#git clone https://huggingface.co/cardiffnlp/twitter-roberta-base-emotion
+
 #docker build -t mikex40/antiviolence-bot -f Dockerfile .
